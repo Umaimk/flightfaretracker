608edaceef495fa83e047461cacee2cc68f5e05f
izanimran <izanimran674@gmail.com>
2026-07-19 04:27:29 +0500

    Updated project file

--- app.py
+++ app.py
@@ -2,9 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
+
     return render_template("index.html")
 
+
+@app.route("/search", methods=["POST"])
+def search():
+
+    return render_template("result")
+
+
+@app.route("/history")
+def history():
+
+    return "History page coming soon"
+
+
+@app.route("/dashboard")
+def dashboard():
+
+    return "Dashboard coming soon"
+
+
+
 if __name__ == "__main__":
+
     app.run(debug=True)
